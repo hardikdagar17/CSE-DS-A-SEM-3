e0ad93a29c6715d0ec398328e18a2f119876c1ff
Hardik Dagar <hardikdagar1803@gmail.com>
2026-09-08 12:11:43 +0530

    Add files via upload

---
+++ StuINFO.js
@@ -0,0 +1,10 @@
+function multiply(a,b) {
+    return a*b;
+}
+function calculate(a,b) {
+    let num1 = prompt("Enter first no. : ");
+    let num2 = prompt("Enter second nunber : ");
+    num1=Number(num1);
+    num2=Number(num2);
+    let 
+}
